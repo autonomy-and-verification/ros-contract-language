74e892e97780fdf84034a05cd6e5e883d9d37ef1
Matt Luckcuck <m.luckcuck@gmail.com>
2020-12-10 17:38:37 +0000

    Basic grammar, parses test contract, test translator outputs the original contract (minus whitespace)

--- rclTrans.py
+++ rclTrans.py
@@ -1,30 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from translators.translator_test import Test_Translator
+from translators.translator_rosmon import ROSMon_Translator
+
 try:
     input = raw_input   # For Python2 compatibility
 except NameError:
     pass
-
-
 from lark import Lark
 import argparse
 
+VERSION_NUM = 0.1
+
+## Arguments
 argParser = argparse.ArgumentParser()
 argParser.add_argument("grammar", help="The grammar file")
 argParser.add_argument("contract", help="The contract file")
+argParser.add_argument("-t", help="The translator to use",choices=['test', 'ros_mon_rml'], default = 'test' )
 argParser.add_argument("-o", help="The output file for the translation")
-#Disabled
+argParser.add_argument("-p", help="The output file for the translation", type=bool, default = False)
 
+
+## Parse the Args
 args = argParser.parse_args()
 
 GRAMMAR = open(args.grammar).read()
 CONTRACT = open(args.contract).read()
+TRANSLATOR = args.t
+PRINT = args.p
+
+print("++++++++++++++++++++++++++++++++++++++++++++++")
+print("++++++ ROS Contract Language Translator ++++++")
+print("++++++++++++++++ version " + str(VERSION_NUM) + " +++++++++++++++++")
+print("+++++++++++++++ Matt Luckcuck ++++++++++++++++")
+print("++++++++++++++++++++++++++++++++++++++++++++++")
+print("")
+
 
 
 
 ## Add the GRAMMAR to the parser
 parser = Lark(GRAMMAR)
 
+#Parse the contract
+parseTree = parser.parse(CONTRACT)
 
-print(parser.parse(CONTRACT).pretty())
+if PRINT:
+    print("+++ Pretty Parse Tree +++")
+    print(parseTree.pretty())
+    print("")
+
+print("+++ Translator Output +++")
+if TRANSLATOR == "test":
+    test_trans = Test_Translator()
+    print(test_trans.translate(parseTree))
+
+elif TRANSLATOR == "ros_mon_rml":
+    romMon_trans = ROSMon_Translator()
+    romMon_trans.translate(parseTree)
+    
+print("")
