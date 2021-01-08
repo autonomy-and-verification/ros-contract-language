9ba245b37421e0854d36906c24805d8ce7f3e76c
Matt Luckcuck <m.luckcuck@gmail.com>
2021-01-08 14:46:20 +0000

    Data model classes for the contract (intermediate representation)

---
+++ contract_model.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Node(object):
+
+    def __init__(self, node_name, topic_list, guarantees):
+        self.node_name  = node_name
+        self.topic_list = topic_list
+        self.guarantees = guarantees
+
+    def get_node_name(self):
+        return self.node_name
+
+    def get_topic_list(self):
+        return self.topic_list
+
+    def get_guarantees(self):
+        return self.guarantees
+
+
+class Contract(object):
+
+    def __init__(self):
+        self.contract_name = ""
+        self.nodes = []
+
+    def set_contract_name(self, name):
+        assert(isinstance(name, str))
+        self.contract_name = name
+
+    def get_contract_name(self):
+        return self.contract_name
+
+    def get_nodes(self):
+        return self.nodes
+
+    def add_node(self, node_name, topic_list, guarantees):
+
+        new_node = Node(node_name, topic_list, guarantees)
+
+        self.nodes.append(new_node)
+
+    def __str__(self):
+        to_string = "contract:" + self.get_contract_name() +"\n"
+
+        if self.get_nodes() != None:
+            for node in self.get_nodes():
+                to_string += "node: " + node.get_node_name() +"\n" + "\ttopics: " + str(node.get_topic_list()) +"\n" + "\tguarantees: " + str(node.get_guarantees())
+
+        return to_string
+
+
+
+if __name__ == "__main__":
+    test_contract = Contract()
+    test_contract.set_contract_name("Test Contract")
+
+    node_name = "Test Node"
+    topic_list = ["bool test", "String data"]
+    guarantees = ["true", "false", "x == y"]
+
+    test_contract.add_node(node_name, topic_list, guarantees)
+
+    print(test_contract)
