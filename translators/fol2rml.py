71ade4449e4139a45e089a029349dfa8708e3e3b
AngeloFerrando <angelo.ferrando42@gmail.com>
2021-02-02 12:46:35 +0100

    negation updated

--- translators/fol2rml.py
+++ translators/fol2rml.py
@@ -4,6 +4,7 @@
 """ Translates FOL to RML, using a Lark Interpreter """
 
 from translators.fol import FOL
+from lark import *
 
 class FOL2RML(FOL):
 
@@ -26,7 +27,7 @@ class FOL2RML(FOL):
     def implies(self, tree):
         """ Translate an implies tree """
         assert(tree.data == "implies")
-        event_type1 = "not_" + self.visit(tree.children[0])
+        event_type1 = self.visit(self.neg(tree.children[0]))
         event_type2 = self.visit(tree.children[1])
         return "(" + event_type1 + " \\/ " + event_type2 + ")"
 
@@ -69,18 +70,19 @@ class FOL2RML(FOL):
     def negation(self, tree):
         """ Translate a negation tree """
         assert(tree.data == "negation")
-        return "not_" + self.visit(tree.children)
+        return self.visit(self.neg(tree.children[0]))
+        # return "not_" + self.visit(tree.children)
 
     def and_form(self, tree):
         """ Translate an and tree """
-        assert(tree.data == "and")
+        assert(tree.data == "and_form")
         event_type1 = self.visit(tree.children[0])
         event_type2 = self.visit(tree.children[1])
         return "(" + event_type1 + " /\\ " + event_type2 + ")"
 
     def or_form(self, tree):
         """ Translate a or tree """
-        assert(tree.data == "or")
+        assert(tree.data == "or_form")
         event_type1 = self.visit(tree.children[0])
         event_type2 = self.visit(tree.children[1])
         return "(" + event_type1 + " \\/ " + event_type2 + ")"
@@ -156,3 +158,44 @@ class FOL2RML(FOL):
         """ Translate a string literal tree """
         assert(tree.data == "string_literal")
         return "'" + tree.children[0] + "'"
+
+    def neg(self, term):
+        if isinstance(term, lexer.Token):
+            return term
+        else:
+            if term.data == "implies":
+                term.data = "and_form"
+                term.children[1] = self.neg(term.children[1])
+            elif term.data == "iff":
+                term.data = "and_form"
+                left = Tree("or_form", [self.neg(term.children[0]),self.neg(term.children[1])])
+                right = Tree("or_form", [term.children[0],term.children[1]])
+                term.children[0] = left
+                term.children[1] = right
+            elif term.data == "and_form":
+                term.data = "or_form"
+                term.children[0] = self.neg(term.children[0])
+                term.children[1] = self.neg(term.children[1])
+            elif term.data == "or_form":
+                term.data = "and_form"
+                term.children[0] = self.neg(term.children[0])
+                term.children[1] = self.neg(term.children[1])
+            elif term.data == "forall":
+                term.data = "exists"
+                term.children[1] = self.neg(term.children[1])
+            elif term.data == "exists":
+                term.data = "forall"
+                term.children[1] = self.neg(term.children[1])
+            elif term.data == "equals":
+                term.data = "not_equals"
+            elif term.data == "not_equals":
+                term.data = "equals"
+            elif term.data == "sub_formula":
+                term.children[0] = self.neg(term.children[0])
+            elif term.data == "negation":
+                term.data = term.children[0].data
+                term.children[0] = term.children[0]
+            elif term.data == "atom":
+                term.children[0] = self.neg(term.children[0])
+
+            return term
