5bd09fafbebef1d2288012c70c40a30d9b04e60c
Matt <m.luckcuck@tutanota.com>
2021-04-02 15:40:27 +0100

    Translates all the agent guarantee!

--- translators/fol2text.py
+++ translators/fol2text.py
@@ -28,8 +28,19 @@ class FOL2Text(FOL):
         def implies(self, tree):
             """ Translate an implies tree """
             assert(tree.data == "implies")
+            print("translating implies:")
+            print(tree)
 
-            return self.visit(tree.children[0]) + " -> " + self.visit(tree.children[1])
+            print("***visiting left..")
+            left = self.visit(tree.children[0])
+
+            print("visiting right..")
+            right = self.visit(tree.children[1])
+
+            print("left = " + left)
+            print("right = " + right)
+
+            return left + " -> " + right
 
         def atom(self, tree):
             """ Translate an atom tree """
@@ -88,36 +99,120 @@ class FOL2Text(FOL):
 
             return in_left + " in " + in_right
 
+        def not_in(self, tree):
+            """ Translate a not in tree """
+            assert(tree.data == "not_in")
+            assert(len(tree.children)==2)
+
+            print("translating a not in")
+
+            in_left = self.visit(tree.children[0])
+            in_right =self.visit(tree.children[1])
+            print("left = " + in_left)
+            print("right = " + in_right)
+
+            return in_left + " !in " + in_right
+
+
         def negation(self, tree):
             """ Translate a negation tree """
             assert(tree.data == "negation")
+            assert(len(tree.children) == 1)
 
             return "not " + self.visit(tree.children[0])
 
         def and_form(self, tree):
             """ Translate an and tree """
-            assert(tree.data == "and")
-            pass
+            assert(tree.data == "and_form")
+            assert(len(tree.children)==2)
+
+            print("translating an and")
+
+            and_left = self.visit(tree.children[0])
+            and_right =self.visit(tree.children[1])
+            print("left = " + and_left)
+            print("right = " + and_right)
+
+            return and_left + " and " + and_right
 
         def or_form(self, tree):
             """ Translate a or tree """
-            assert(tree.data == "or")
-            pass
+            assert(tree.data == "or_form")
+            assert(len(tree.children)==2)
+
+            print("translating an or")
+
+            or_left = self.visit(tree.children[0])
+            or_right =self.visit(tree.children[1])
+            print("left = " + or_left)
+            print("right = " + or_right)
+
+            return or_left + " and " + or_right
 
         def iff(self, tree):
             """ Translate a iff tree """
             assert(tree.data == "iff")
-            pass
+            assert(len(tree.children)==2)
+
+            print("translating an iff")
+
+            iff_left = self.visit(tree.children[0])
+            iff_right =self.visit(tree.children[1])
+            print("left = " + iff_left)
+            print("right = " + iff_right)
+
+            return iff_left + " <=> " + iff_right
 
         def forall(self, tree):
             """ Translate a forall tree """
             assert(tree.data == "forall")
-            pass
+            assert(len(tree.children) == 2 )
+
+            print("translating forall")
+
+            #"forall" "(" variables (";"|"|") formula ")
+            variables = tree.children[0]
+            formula = tree.children[1]
+
+            variables_out = self.visit(variables)
+            formula_out = self.visit(formula)
+
+            return "forall (" + variables_out + " | " + formula_out + ")"
 
         def exists(self, tree):
             """ Translate a exists tree """
             assert(tree.data == "exists")
-            pass
+            assert(len(tree.children) == 2 )
+            print("trnaslating an exists tree")
+            #"exists" "(" variables (";"|"|") formula ")"
+
+            variables = tree.children[0]
+            formula = tree.children[1]
+
+            variables_out = self.visit(variables)
+            formula_out = self.visit(formula)
+
+            print("Exists...")
+            print(variables_out)
+            print(formula_out)
+
+            return "exists (" + variables_out + " | " + formula_out + ")"
+
+
+        def exists_unique(self, tree):
+            """ Translate a exists_unique tree """
+            assert(tree.data == "exists_unique")
+            assert(len(tree.children) == 2 )
+            print("trnaslating an exists_unique tree")
+            #"exists" "(" variables (";"|"|") formula ")"
+
+            variables = tree.children[0]
+            formula = tree.children[1]
+
+            variables_out = self.visit(variables)
+            formula_out = self.visit(formula)
+
+            return "exists_unique (" + variables_out + " | " + formula_out + ")"
 
         def predicate(self, tree):
             """ Translate a predicate tree """
@@ -147,21 +242,50 @@ class FOL2Text(FOL):
 
             return str(tree.children[0])
 
+        def arithmetic(self, tree):
+            """Translate an arithmetic statement """
+            assert(tree.data == "arithmetic")
+            assert(len(tree.children) == 1)
+
+            print("translating an arithmetic statement")
+            print(tree)
+            arith_tree = tree.children[0]
+            assert(arith_tree.data == "arith")
+
+            #(VARIABLE|NUMBER) ARITH_OP (VARIABLE|NUMBER)
+
+            left = str(arith_tree.children[0])
+            op = str(arith_tree.children[1])
+            right = str(arith_tree.children[2])
+
+            return left + op + right
+
+
         def set(self, tree):
             """Translates a set tree """
             assert(tree.data == "set")
 
             print("translating a set")
+            print(tree)
 
             head, *tail = tree.children
 
             vars = str(head)
 
             for var in tail:
+                print(var)
                 vars += ", " + str(var)
+
+            print(type(vars))
+            print(vars)
 
             return "{" + vars + "}"
 
+        def tuple(self, tree):
+            """Translates a tuple tree """
+            print("Implement Tuple")
+
+            pass
 
         def function(self, tree):
             """ Translate a function tree """
@@ -173,8 +297,40 @@ class FOL2Text(FOL):
 
             return func_name + "("+ str(func_args) +")"
 
+        def terms(self, tree):
+            """ Translate a terms tree """
+            assert(tree.data == "terms")
+
+            print("translating terms")
+
+            head, *tail = tree.children
+            terms_out = self.visit(head)
+
+            for term in tail:
+                terms_out += ", " + self.visit(term) 
+
+            return terms_out
+
         def string_literal(self, tree):
             """ Translate a string literal tree """
             assert(tree.data == "string_literal")
 
             return "\"" + tree.children[0] + "\""
+
+        def variables(self, tree):
+            """ Translate a variables tree """
+            assert(tree.data == "variables")
+
+            print("transaling variables tree")
+
+            print(tree)
+
+            head, *tail = tree.children
+
+            vars = str(head)
+
+            for var in tail:
+                print(var)
+                vars += ", " + str(var)
+
+            return vars
