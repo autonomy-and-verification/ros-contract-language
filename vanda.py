2e19a47b2e18547df4cb429aeb9c82a7b213e011
Matt Luckcuck <m.luckcuck@gmail.com>
2021-08-16 12:15:57 +0100

    Tweasks for PEP warnings

--- vanda.py
+++ vanda.py
@@ -19,12 +19,17 @@ VERSION_NUM = 0.1
 
 ## Arguments
 argParser = argparse.ArgumentParser(prog='Vanda')
-argParser.add_argument("grammar", help="The grammar to parse with.", default = "rcl")
+argParser.add_argument(
+    "grammar", help="The grammar to parse with.", default="rcl")
 argParser.add_argument("contract", help="The contract file to be parsed.")
-argParser.add_argument("-t", help="The translator to use",choices=['mirror', 'rosmon_rml', 'latex'], default = 'mirror' )
-argParser.add_argument("-o", help="The path to the output folder for the translation")
-argParser.add_argument("-p", help="Print the parse tree", type=bool, default = False)
-argParser.add_argument('--version', action='version', version='%(prog)s ' + str(VERSION_NUM))
+argParser.add_argument("-t", help="The translator to use",
+                       choices=['mirror', 'rosmon_rml', 'latex'], default='mirror')
+argParser.add_argument(
+    "-o", help="The path to the output folder for the translation")
+argParser.add_argument("-p", help="Print the parse tree",
+                       type=bool, default=False)
+argParser.add_argument('--version', action='version',
+                       version='%(prog)s ' + str(VERSION_NUM))
 
 
 print("++++++++++++++++++++++++++++++++++++++++++++++")
@@ -35,10 +40,10 @@ print("+++++++++++++++ Matt Luckcuck ++++++++++++++++")
 print("++++++++++++++++++++++++++++++++++++++++++++++")
 print("")
 
-## Parse the Args
+# Parse the Args
 args = argParser.parse_args()
 
-grammar_loc = "grammars/" + args.grammar +".lark"
+grammar_loc = "grammars/" + args.grammar + ".lark"
 
 GRAMMAR = open(grammar_loc).read()
 CONTRACT = open(args.contract).read()
@@ -60,14 +65,14 @@ TRANSLATOR = args.t
 PRINT = args.p
 
 
-## Add the GRAMMAR to the parser
+# Add the GRAMMAR to the parser
 parser = Lark(GRAMMAR)
 
 print("+++ Input File = +++")
 print(CONTRACT)
 print("")
 
-#Parse the contract
+# Parse the contract
 parse_tree = parser.parse(CONTRACT)
 
 if PRINT:
@@ -94,14 +99,14 @@ print("")
 
 if TRANSLATOR == "mirror":
     # Just prints the output of the Mirror translator
-    #, which should be the same (apart from whitespace) as the input
+    # , which should be the same (apart from whitespace) as the input
     test_trans = Mirror()
     print(test_trans.translate(contract_obj))
 
 elif TRANSLATOR == "rosmon_rml":
 
     rosMon = ROSMon_Translator(contract_obj)
-    #rosmon_config = romMon.translate(parse_tree)
+    # rosmon_config = romMon.translate(parse_tree)
 
     rosmon_config, rml = rosMon.translate_config()
 
@@ -123,13 +128,14 @@ elif TRANSLATOR == "latex":
     latex_output = latex_translator.translate(contract_obj)
     print(latex_output)
 
-    latex_main_file = "\\documentclass[12pt,a4paper]{article}\n\\usepackage[utf8]{inputenc}\n\\usepackage[english]{babel}\n\\usepackage{amsmath}\n\\usepackage{lmodern}\n\\usepackage[left=4cm,right=4cm,top=4cm,bottom=4cm]{geometry}\n\\usepackage[UKenglish]{isodate}\n\\author{Vanda v"+str(VERSION_NUM)+"}\n\\title{Contract for: "+CONTRACT_NAME+"}\\begin{document}\n\\maketitle\n\n\input{"+  CONTRACT_NAME+".tex}\n\\end{document}"
+    latex_main_file = "\\documentclass[12pt,a4paper]{article}\n\\usepackage[utf8]{inputenc}\n\\usepackage[english]{babel}\n\\usepackage{amsmath}\n\\usepackage{lmodern}\n\\usepackage[left=4cm,right=4cm,top=4cm,bottom=4cm]{geometry}\n\\usepackage[UKenglish]{isodate}\n\\author{Vanda v"+str(
+        VERSION_NUM)+"}\n\\title{Contract for: "+CONTRACT_NAME+"}\\begin{document}\n\\maketitle\n\n\input{" + CONTRACT_NAME+".tex}\n\\end{document}"
 
-    output_file = open( OUTPUT_PATH+CONTRACT_NAME+".tex", "w")
+    output_file = open(OUTPUT_PATH+CONTRACT_NAME+".tex", "w")
     output_file.write(latex_output)
     output_file.close()
 
-    output_main_file = open( OUTPUT_PATH+CONTRACT_NAME+"-main.tex", "w")
+    output_main_file = open(OUTPUT_PATH+CONTRACT_NAME+"-main.tex", "w")
     output_main_file.write(latex_main_file)
     output_main_file.close()
 
